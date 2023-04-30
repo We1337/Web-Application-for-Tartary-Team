633af1121c5a5196024645c43dde669febc5eb77
We1337 <githubdevice@gmail.com>
2023-04-30 19:28:15 +0500

    refactor: unnecessary code keeping for letter feater

--- js/markdown.js
+++ js/markdown.js
@@ -1,6 +1,8 @@
+/*
 fetch('https://raw.githubusercontent.com/We1337/Roadmap-study/main/Cyber%20Security/Clueless/Clueless.md').then(response => response.text()).then(data => {
     let converter = new showdown.Converter();
     let md = data;
     let html = converter.makeHtml(md);
     document.querySelector('#markdown-container').innerHTML = html;
 })
+*/
